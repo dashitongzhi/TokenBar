7c8728f7faabd5425a96356cfb94c788e84de414
dashitongzhi <civilization.cn@outlook.com>
2026-05-11 21:22:50 +0800

    Initial Commit

---
+++ TokenBar/TokenBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenBarApp.swift
+//  TokenBar
+//
+//  Created by Kral on 2026/5/11.
+//
+
+import SwiftUI
+
+@main
+struct TokenBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
